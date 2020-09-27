bf710af7abd482c5593c81b2122bbdec4982b35d
Otgooae <68878212+Otgooae@users.noreply.github.com>
2020-09-27 18:22:36 -0400

    added unit conversion

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import ReactAnimatedWeather from "react-animated-weather";
 import Forecast from "./Forecast";
+import TempUnit from "./TempUnit";
 export default function SearchEngine(props) {
   let [city, setCity] = useState(props.city);
   let [weather, setWeather] = useState({
@@ -13,13 +14,15 @@ export default function SearchEngine(props) {
     setWeather({
       cityName: response.data.name,
       temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
     });
   }
   function showCity(event) {
     event.preventDefault();
     let apiKey = "a65dcb5eff2739b507931b36f4d9c30c";
     let unit = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&unit=${unit}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
     axios.get(apiUrl).then(displayCityTemp);
   }
 
@@ -35,7 +38,9 @@ export default function SearchEngine(props) {
         size={150}
         animate={true}
       />
-      <h3 className="currentTemp">{weather.temperature}</h3>
+      <h3 className="currentTemp">
+        <TempUnit temperature={weather.temperature} />
+      </h3>
       <ul className="weatherDetails">
         <li>Humidity:{weather.humidity}</li>
         <li>Wind:{weather.wind}</li>
---
+++ src/TempUnit.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export default function TempUnit(props) {
+  const [temperature, setTemperature] = useState(null);
+  function setCelsiusTemp(event) {
+    event.preventDefault();
+    setTemperature(props.temperature);
+  }
+  function setFahrenheitTemp(event) {
+    event.preventDefault();
+    setTemperature(Math.round((props.temperature * 9) / 5 + 32));
+  }
+  return (
+    <div className="TempUnit">
+      {temperature}
+      <a href="/" className="celsiusTemp" onClick={setCelsiusTemp}>
+        °C
+      </a>{" "}
+      |{" "}
+      <a href="/" className="FahrenheitTemp" onClick={setFahrenheitTemp}>
+        F
+      </a>
+    </div>
+  );
+}
