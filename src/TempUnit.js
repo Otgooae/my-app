b411749afff856138fba0629f33bafab2e3fb07b
Otgooae <68878212+Otgooae@users.noreply.github.com>
2020-09-29 21:25:32 -0400

    updated tempUnit.js

--- src/TempUnit.js
+++ src/TempUnit.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function TempUnit(props) {
   return (
     <div className="TempUnit">
       <h2>{props.temperature}</h2>
       <div className="Units">
-        <a href="/" className="celsius">
+        <a href="/" className="celsius" rel="noopener noreferrer">
           °C
         </a>{" "}
         |
-        <a href="/" className="FahrenheitTemp">
+        <a href="/" className="FahrenheitTemp" rel="noopener noreferrer">
           F
         </a>
       </div>
