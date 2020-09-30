500e794c818c635a3307524ba74fb0b07d541537
Otgooae <68878212+Otgooae@users.noreply.github.com>
2020-09-29 21:30:26 -0400

    added git link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
         <header className="App-header">
           <SearchEngine />
         </header>
+        <div className="gitLink">
+          <a
+            href="https://github.com/Otgooae/my-app.git"
+            rel="noopener noreferrer"
+          >
+            Open source code by Otgoo Altangerel
+          </a>
+        </div>
       </div>
     </div>
   );
