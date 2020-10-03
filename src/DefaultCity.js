72eff99c2e893beb19ad7d6232c20b53648deb0b
Otgooae <68878212+Otgooae@users.noreply.github.com>
2020-10-03 15:13:31 -0400

    added manifest contents back

--- src/DefaultCity.js
+++ src/DefaultCity.js
@@ -5,14 +5,14 @@ import CurrentDate from "./CurrentDate";
 import WeatherIcon from "./WeatherIcon";
 import "./DefaultCity.css";
 export default function DefaultCity(props) {
-  let weather = props.data;
+  let weather = props.data
   return (
     <div className="DefaultCity">
       <h1 className="cityName">{weather.cityName}</h1>
       <h2>
         <CurrentDate date={weather.date} />
       </h2>
-      <WeatherIcon icon={weather.icon}/>
+      <WeatherIcon icon={weather.icon} />
       <div className="description">
         <small className="text-capitalize">{weather.description}</small>
       </div>
@@ -27,26 +27,6 @@ export default function DefaultCity(props) {
             <li>Humidity: {weather.humidity}%</li>
             <li>Wind: {weather.wind}km/hr</li>
           </ul>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-2">
-          <Forecast forecastCity={props.city} time="12:00" index={0} />
-        </div>
-        <div className="col-2">
-          <Forecast forecastCity={props.city} time="12:00" index={1} />
-        </div>
-        <div className="col-2">
-          <Forecast forecastCity={props.city} time="12:00" index={2} />
-        </div>
-        <div className="col-2">
-          <Forecast forecastCity={props.city} time="12:00" index={3} />
-        </div>
-        <div className="col-2">
-          <Forecast forecastCity={props.city} time="12:00" index={4} />
-        </div>
-        <div className="col-2">
-          <Forecast forecastCity={props.city} time="12:00" index={5} />
         </div>
       </div>
     </div>
